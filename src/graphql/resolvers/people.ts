a920b38c2fb961035eeedab242c52a27956220e4
Juan  E Angulo U <jangulorama@gmail.com>
2023-05-31 08:17:36 -0500

    Added new mutations and  methods to search

--- src/graphql/resolvers/people.ts
+++ src/graphql/resolvers/people.ts
@@ -1,10 +1,41 @@
-import {IResolvers} from '@graphql-tools/utils';
+import { IResolvers } from '@graphql-tools/utils';
 import { peopleDataSource } from '../../data/peopledata';
 
 const peopleResolver: IResolvers = {
-    Query: {        
-        getPeople(){
-            return peopleDataSource;
+    Query: {
+        getPeople: () => peopleDataSource,
+        getPersonByName: (parent, { name }) => {
+            return peopleDataSource.filter(peopleDataSource => peopleDataSource.name.toLowerCase().includes(name.toLowerCase()));
+        }
+    },
+    Mutation: {
+        createPerson: (parent, { input }) => {
+            const newPerson = {
+                id: String(peopleDataSource.length + 1),
+                ...input
+            };
+            peopleDataSource.push(newPerson);
+            return newPerson;
+        },
+        updatePerson: (parent, { id, input }) => {
+            const personIndex = peopleDataSource.findIndex(person => person.id === id);
+            if (personIndex !== -1) {
+                const updatedPerson = {
+                    id,
+                    ...input
+                };
+                peopleDataSource[personIndex] = updatedPerson;
+                return updatedPerson;
+            }
+            throw new Error("Person not found");
+        },
+        deletePerson: (parent, { id }) => {
+            const personIndex = peopleDataSource.findIndex(person => person.id === id);
+            if (personIndex !== -1) {
+                peopleDataSource.splice(personIndex, 1);
+                return true;
+            }
+            throw new Error("Person not found");
         }
     }
 }
